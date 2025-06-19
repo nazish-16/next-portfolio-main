ccc536bdfa9178dad0ec0ac84e1da8785dc2bfff
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2025-06-19 18:15:35 +0530

    script removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,22 +21,6 @@ const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-(function() {
-  try {
-    var theme = localStorage.getItem('theme');
-    if (!theme) {
-      theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    }
-    document.documentElement.classList.remove('light', 'dark');
-    document.documentElement.classList.add(theme);
-  } catch (e) {}
-})();
-            `,
-          }}
-        />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
