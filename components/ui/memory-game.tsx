e3c2ab17ebec7b715d2dd20823c70600d8017a42
Frezz1ck <84974655+nazish-16@users.noreply.github.com>
2025-06-19 18:21:28 +0530

    added use client

--- components/ui/memory-game.tsx
+++ components/ui/memory-game.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 
--- components/ui/progress-graph.tsx
+++ components/ui/progress-graph.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const generateYearData = () => {
